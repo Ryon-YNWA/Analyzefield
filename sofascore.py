234cfcdee0a77a7706038582c97297ad80eac429
Ryon-YNWA <ryon.ynwa@gmail.com>
2022-01-18 22:57:45 +0900

    add player statistics scraping function

--- sofascore.py
+++ sofascore.py
@@ -11,11 +11,45 @@ class Sofascore(scrape.Scraper):
         super().__init__()
         self._update_url(url)
         self._change_window_size(1200, 400)
+        self.__parser = 'html.parser'
+
+    def __get_df_player_statistics(self):
+
+        soup = BeautifulSoup(self._driver.page_source.encode('utf-8'),  self.__parser)
+        table = soup.find('table', {'class': 'styles__Table-sc-17k0iju-6 eApptp'})
+        return pd.read_html(str(table))
+
+    def __scrape_player_statistics(self):
+
+        # Player Statisticsを選択
+        self._driver.find_element(by=By.XPATH, value='//a[text()="Player statistics"]').click()
+
+        # Player Statisticsを順にdataframeで取得
+        self._driver.find_element(by=By.XPATH, value='//a[text()="Summary"]').click()
+        df_summary = self.__get_df_player_statistics()
+
+        self._driver.find_element(by=By.XPATH, value='//a[text()="Attack"]').click()
+        df_attack= self.__get_df_player_statistics()
+
+        self._driver.find_element(by=By.XPATH, value='//a[text()="Defence"]').click()
+        df_defence = self.__get_df_player_statistics()
+
+        self._driver.find_element(by=By.XPATH, value='//a[text()="Passing"]').click()
+        df_passing = self.__get_df_player_statistics()
+
+        self._driver.find_element(by=By.XPATH, value='//a[text()="Duels"]').click()
+        df_duels = self.__get_df_player_statistics()
+
+        self._driver.find_element(by=By.XPATH, value='//a[text()="Goalkeeper"]').click()
+        df_goalkeeper = self.__get_df_player_statistics()
 
     def run(self):
         """mainプロセス"""
 
         self._screenshot('snap')
+
+        # Player Statisticsを選択
+        self.__scrape_player_statistics()
 
         self._close_driver()
 
